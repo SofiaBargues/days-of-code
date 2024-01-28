4dff1f080e8dc152bc61bb80e11fb7eab069b9b9
Sofia Bargues <barguessofia@gmail.com>
2024-01-28 16:58:48 +0000

    es algo

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,47 +1,103 @@
 export default [
   {
-    img: "/images/1.jpg",
+    img: "/images/1.png",
     title: "Create a website using Html CSS and JavaScript",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    img: "/images/2.jpg",
+    img: "/images/2.png",
     title: "Complete portfolio website tutorial",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    img: "/images/3.jpg",
+    img: "/images/3.png",
     title: "Bootstrap 5 Complete tutorial",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    img: "/images/4.jpg",
+    img: "/images/4.png",
     title: "UIkit Complete tutorial",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    img: "/images/5.jpg",
+    img: "/images/5.png",
     title: "Tailwind CSS card design",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    img: "/images/6.jpg",
+    img: "/images/6.png",
     title: "Reactjs tutorial for beginner",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    img: "/images/7.jpg",
+    img: "/images/7.png",
     title: "Nextjs crash course",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    img: "/images/8.jpg",
+    img: "/images/8.png",
     title: "Create a website using Tailwind CSS",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    img: "/images/9.jpg",
+    img: "/images/9.png",
     title: "Create an one page website using ReactJS",
-    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/10.png",
+    title: "Create an one page website using ReactJS",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+
+  {
+    img: "/images/11.png",
+    title: "Tailwind CSS card design",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/12.png",
+    title: "Reactjs tutorial for beginner",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/13.png",
+    title: "Nextjs crash course",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/14.jpg",
+    title: "Create a website using Tailwind CSS",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/15.png",
+    title: "Create an one page website using ReactJS",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/16.png",
+    title: "Create an one page website using ReactJS",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/17.png",
+    title: "Create an one page website using ReactJS",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/18.png",
+    title: "Create an one page website using ReactJS",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/19.png",
+    title: "Create an one page website using ReactJS",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    img: "/images/20.png",
+    title: "Create an one page website using ReactJS",
+    link: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
 ];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,45 @@
-import Image from "next/image";
 import cardList from "./data";
+
+function Card({
+  img,
+  title,
+  day,
+  link,
+}: {
+  img: string;
+  title: string;
+  day: number;
+  link: string;
+}) {
+  return (
+    <a
+      href={link}
+      className="card w-full bg-base-100 rounded-none border hover:opacity-70"
+    >
+      <div className="card-body items-center text-center gap-4">
+        <h3 className="text-5xl text-right w-full">{day}</h3>
+        <h2 className="card-title">{title}</h2>
+      </div>
+      <figure className="">
+        <img src={img} alt="" className="" />
+      </figure>
+    </a>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="container mx-auto py-36 px-8">
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
+    <main className="container mx-auto py-10 px-8 flex flex-col gap-10">
+      <h1 className="text-6xl font-bold text-center ">30 Days of Code</h1>
+      <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 ">
         {cardList.map((card, index) => (
-          <div className="shadow-lg rounded-lg" key={index}>
-            <img className="rounded-t-lg" src={card.img} alt="" />
-            <div className="p-5">
-              <h3 className="text-3xl font-bold text-slate-700 mb-3">
-                {card.title}
-              </h3>
-              <p className="text-lg font-normal text-gray-600 ">{card.text}</p>
-            </div>
-          </div>
+          <Card
+            img={card.img}
+            link={card.link}
+            title={card.title}
+            key={index}
+            day={index + 1}
+          />
         ))}
       </div>
     </main>
